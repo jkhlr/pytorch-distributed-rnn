1e354f476a3c2d349aa7cc7a2afd8e4aa0391c74
Torben Meyer <torben.meyer@bakdata.com>
2020-05-23 13:44:11 +0200

    Update model

--- src/corona/model.py
+++ src/corona/model.py
@@ -7,19 +7,16 @@ class CoronaVirusPredictor(nn.Module):
         self.n_hidden = n_hidden
         self.seq_len = seq_len
         self.n_layers = n_layers
-        self.lstm = nn.LSTM(input_size=n_features, hidden_size=n_hidden, num_layers=n_layers, dropout=0.1)
+        self.lstm = nn.LSTM(input_size=n_features, hidden_size=n_hidden, num_layers=n_layers, batch_first=True)
         self.linear = nn.Linear(in_features=n_hidden, out_features=1)
-        self.hidden = self.__initial_hidden_state()
 
-    def reset_hidden_state(self):
-        self.hidden = self.__initial_hidden_state()
-
-    def forward(self, sequences: torch.FloatTensor):
-        lstm_out, self.hidden = self.lstm(sequences.view(len(sequences), self.seq_len, -1), self.hidden)
-        last_time_step = lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
-        y_pred = self.linear(last_time_step)
+    def forward(self, sequences):
+        batch_size = sequences.size(0)
+        h0, c0 = self._initial_hidden_state(batch_size)
+        lstm_out, self.hidden = self.lstm(sequences, (h0, c0))
+        y_pred = self.linear(lstm_out[:, -1, :])
         return y_pred
 
-    def __initial_hidden_state(self) -> (torch.FloatTensor, torch.FloatTensor):
-        return (torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
-                torch.zeros(self.n_layers, self.seq_len, self.n_hidden))
+    def _initial_hidden_state(self, batch_size) -> (torch.FloatTensor, torch.FloatTensor):
+        return (torch.zeros(self.n_layers, batch_size, self.n_hidden),
+                torch.zeros(self.n_layers, batch_size, self.n_hidden))
