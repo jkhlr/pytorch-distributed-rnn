7323a936485acd82bd380709bc63e0a59c1b66a0
Torben Meyer <torben.meyer@bakdata.com>
2020-05-23 13:43:25 +0200

    Add main

--- src/corona/main.py
+++ src/corona/main.py
@@ -1,28 +1,34 @@
-from os.path import dirname, realpath
 from pathlib import Path
-from torchnet.dataset import ShuffleDataset
 
 from dataset import CoronaDataset
 from model import CoronaVirusPredictor
+from torchnet.dataset import ShuffleDataset
 from trainer import DDPTrainer
 
 SCRIPT_DIR = Path(__file__).absolute().parent
 CHECKPOINT_DIR = SCRIPT_DIR / 'models'
 DATASET_PATH = SCRIPT_DIR / 'data' / 'train.csv'
 
-dataset = CoronaDataset.load(DATASET_PATH)
-training_set, validation_set = dataset.random_split()
 
-model = CoronaVirusPredictor(
-    n_features=training_set.num_features,
-    seq_len=training_set.seq_length,
-    n_hidden=128,
-    n_layers=3
-)
+def main():
+    dataset = CoronaDataset.load(DATASET_PATH)
+    training_set, validation_set = dataset.random_split()
 
-trainer = DDPTrainer(
-    model=model,
-    training_set=ShuffleDataset(training_set),
-    checkpoint_dir=CHECKPOINT_DIR
-)
-trained_model, history = trainer.train(epochs=50)
+    model = CoronaVirusPredictor(
+        n_features=dataset.num_features,
+        seq_len=dataset.seq_length,
+        n_hidden=1024,
+        n_layers=1
+    )
+
+    trainer = DDPTrainer(
+        model=model,
+        training_set=ShuffleDataset(training_set),
+        validation_set=validation_set,
+        checkpoint_dir=CHECKPOINT_DIR
+    )
+    trained_model, history = trainer.train(epochs=50)
+
+
+if __name__ == '__main__':
+    main()
